6abbc7b01feea0944dda29c189c4752a7dfc95d7
Leo <leoc2628@gmail.com>
2022-08-05 17:29:39 -0400

    init latest version with vite

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import App from './App.svelte';
+import './app.css'
+import App from './App.svelte'
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'there'
-	}
-});
+  target: document.getElementById('app')
+})
 
-export default app;
+export default app

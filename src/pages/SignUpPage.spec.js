fca915f213248d0a8dcc9ebc9f52a4ee3e5ed697
Leo <leoc2628@gmail.com>
2022-08-05 17:34:54 -0400

    add sign up page header test

---
+++ src/pages/SignUpPage.spec.js
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/svelte';
+import SignUpPage from './SignUpPage.svelte';
+
+it('has sign up header', () => {
+  render(SignUpPage);
+
+  const header = screen.getByRole('heading', { name: 'Sign Up' });
+  expect(header).toBeTruthy();
+});

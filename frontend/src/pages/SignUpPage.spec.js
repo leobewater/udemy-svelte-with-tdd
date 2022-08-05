9f38c5cccaa6ef3a617800c66da5643b445d596d
Leo <leoc2628@gmail.com>
2022-08-05 18:10:01 -0400

    add basic input tests

--- frontend/src/pages/SignUpPage.spec.js
+++ frontend/src/pages/SignUpPage.spec.js
@@ -2,9 +2,25 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/svelte';
 import SignUpPage from './SignUpPage.svelte';
 
-it('has sign up header', () => {
-  render(SignUpPage);
+describe('Sign Up Page', () => {
+  describe('Layout', () => {
+    it('has sign up header', () => {
+      render(SignUpPage);
 
-  const header = screen.getByRole('heading', { name: 'Sign Up' });
-  expect(header).toBeInTheDocument();
+      const header = screen.getByRole('heading', { name: 'Sign Up' });
+      expect(header).toBeInTheDocument();
+    });
+
+    it('has username input', () => {
+      const { container } = render(SignUpPage);
+      const input = container.querySelector('input');
+      expect(input).toBeInTheDocument();
+    });
+
+    it('has email input', () => {
+      const { container } = render(SignUpPage);
+      const input = container.querySelector('input');
+      expect(input).toBeInTheDocument();
+    });
+  });
 });

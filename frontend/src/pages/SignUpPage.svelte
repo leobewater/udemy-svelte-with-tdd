f5f9f6c0801dd74f9650be7906c509741199aaf4
Leo <leoc2628@gmail.com>
2022-08-06 15:57:42 -0400

    refactor bind logic

--- frontend/src/pages/SignUpPage.svelte
+++ frontend/src/pages/SignUpPage.svelte
@@ -1,26 +1,21 @@
 <script>
-  let disabled = true;
   let password, passwordRepeat;
 
-  const onChangePassword = (event) => {
-    password = event.target.value;
+  //   const onChangePassword = (event) => {
+  //     password = event.target.value;
+  //     // refreshDisabled();
+  //   };
 
-    if (password === passwordRepeat) {
-      disabled = false;
-    } else {
-      disabled = true;
-    }
-  };
+  //   const onChangePasswordRepeat = (event) => {
+  //     passwordRepeat = event.target.value;
+  //     // refreshDisabled();
+  //   };
 
-  const onChangePasswordRepeat = (event) => {
-    passwordRepeat = event.target.value;
+  //   const refreshDisabled = () => {
+  //     disabled = password !== passwordRepeat;
+  //   };
 
-    if (password === passwordRepeat) {
-      disabled = false;
-    } else {
-      disabled = true;
-    }
-  };
+  $: disabled = password && passwordRepeat ? password !== passwordRepeat : true;
 </script>
 
 <h1>Sign Up</h1>
@@ -31,9 +26,10 @@
 <input id="e-mail" />
 
 <label for="password">Password</label>
-<input id="password" type="password" on:input={onChangePassword} />
+<input id="password" type="password" bind:value={password} />
+<!-- <input id="password" type="password" on:input={(event) => (password = event.target.value)} /> -->
 
 <label for="password-repeat">Password Repeat</label>
-<input id="password-repeat" type="password" on:input={onChangePasswordRepeat} />
+<input id="password-repeat" type="password" bind:value={passwordRepeat} />
 
 <button {disabled}>Sign Up</button>

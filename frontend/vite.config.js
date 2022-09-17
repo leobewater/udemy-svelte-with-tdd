b3e3cd30f076dbb327a2b8325329730a9c3af67d
Leo <leoc2628@gmail.com>
2022-09-17 17:11:36 -0400

    add local proxy for api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    proxy: {
+      '^/api/*' :{
+        target: process.env.SERVER_URL,
+        changeOrigin: true,
+        secure: false,
+        // rewrite: path => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })

e0aeb426fea672e686de132e30e6d2ffcefd6d49
Leo <leoc2628@gmail.com>
2022-09-17 16:18:19 -0400

    using fetch instead of axios

--- frontend/src/pages/SignUpPage.svelte
+++ frontend/src/pages/SignUpPage.svelte
@@ -1,5 +1,7 @@
 <script>
-  let password, passwordRepeat;
+  // import axios from 'axios';
+  let disabled = true;
+  let username, email, password, passwordRepeat;
 
   //   const onChangePassword = (event) => {
   //     password = event.target.value;
@@ -16,20 +18,41 @@
   //   };
 
   $: disabled = password && passwordRepeat ? password !== passwordRepeat : true;
+
+  const submit = () => {
+    // axios.post('/api/1.0/users', {
+    //   username,
+    //   email,
+    //   password,
+    // });
+    fetch('/api/1.0/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+  };
 </script>
 
 <h1>Sign Up</h1>
-<label for="username">Username</label>
-<input id="username" />
+<form on:submit|preventDefault={submit}>
+  <label for="username">Username</label>
+  <input id="username" bind:value={username} />
 
-<label for="e-mail">Email</label>
-<input id="e-mail" />
+  <label for="e-mail">Email</label>
+  <input id="e-mail" bind:value={email} />
 
-<label for="password">Password</label>
-<input id="password" type="password" bind:value={password} />
-<!-- <input id="password" type="password" on:input={(event) => (password = event.target.value)} /> -->
+  <label for="password">Password</label>
+  <input id="password" type="password" bind:value={password} />
+  <!-- <input id="password" type="password" on:input={(event) => (password = event.target.value)} /> -->
 
-<label for="password-repeat">Password Repeat</label>
-<input id="password-repeat" type="password" bind:value={passwordRepeat} />
+  <label for="password-repeat">Password Repeat</label>
+  <input id="password-repeat" type="password" bind:value={passwordRepeat} />
 
-<button {disabled}>Sign Up</button>
+  <button {disabled}>Sign Up</button>
+</form>

e921a27563af2220665a5c64d4e1d3b28b1711a3
Leo <leoc2628@gmail.com>
2022-09-17 17:48:22 -0400

    fix proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   server: {
     proxy: {
-      '^/api/*' :{
-        target: process.env.SERVER_URL,
+      '^/api/.*': {
+        target: 'http://localhost:8080',
         changeOrigin: true,
-        secure: false,
-        // rewrite: path => path.replace(/^\/api/, ''),
-      }
-    }
-  }
-})
+        // rewrite: (path) => {
+        //   console.log(path);
+        //   const p = path.replace(/^\/api/, '/api');
+        //   console.log(p);
+        //   return path;
+        // },
+      },
+    },
+  },
+});
